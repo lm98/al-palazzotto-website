9e3a324955cb83bc4774e41817a138875e03aa89
LEONARDO MICELLI <leonardomicelli@gmail.com>
2025-02-07 16:59:49 +0100

    fix: import images to bundle

--- src/App.tsx
+++ src/App.tsx
@@ -4,58 +4,75 @@ import Header from "./components/Header";
 import ApartmentSection from "./components/ApartmentSection";
 import Footer from "./components/Footer";
 
+// Imports for "Appartamento Maestrale" images
+import a1i1 from "./assets/a1i1.jpg";
+import a1i2 from "./assets/a1i2.jpg";
+import a1i3 from "./assets/a1i3.jpg";
+import a1i4 from "./assets/a1i4.jpg";
+import a1i5 from "./assets/a1i5.jpg";
+import a1i6 from "./assets/a1i6.jpg";
+import a1i7 from "./assets/a1i7.jpg";
+import a1i8 from "./assets/a1i8.jpg";
+import a1i9 from "./assets/a1i9.jpg";
+import a1i10 from "./assets/a1i10.jpg";
+
+// Imports for "Appartamento Scirocco" images
+import a2i1 from "./assets/a2i1.jpg";
+import a2i2 from "./assets/a2i2.jpg";
+import a2i3 from "./assets/a2i3.jpg";
+import a2i4 from "./assets/a2i4.jpg";
+import a2i5 from "./assets/a2i5.jpg";
+import a2i6 from "./assets/a2i6.jpg";
+import a2i7 from "./assets/a2i7.jpg";
+import a2i8 from "./assets/a2i8.jpg";
+import a2i9 from "./assets/a2i9.jpg";
+import a2i10 from "./assets/a2i10.jpg";
+
+// Imports for "Palazzotto Levante" images
+import a3i1 from "./assets/a3i1.jpg";
+import a3i2 from "./assets/a3i2.jpg";
+import a3i3 from "./assets/a3i3.jpg";
+import a3i4 from "./assets/a3i4.jpg";
+import a3i5 from "./assets/a3i5.jpg";
+import a3i6 from "./assets/a3i6.jpg";
+import a3i7 from "./assets/a3i7.jpg";
+import a3i8 from "./assets/a3i8.jpg";
+import a3i9 from "./assets/a3i9.jpg";
+import a3i10 from "./assets/a3i10.jpg";
+import a3i11 from "./assets/a3i11.jpg";
+import a3i12 from "./assets/a3i12.jpg";
+import a3i13 from "./assets/a3i13.jpg";
+
 const apartments: { title: string; description: string; images: string[] }[] = [
   {
     title: "Appartamento Maestrale",
     description:
       "Appartamento da due posti letto, con bagno privato, cucina e spazio esterno.",
-    images: [
-      "src/assets/a1i1.jpg",
-      "src/assets/a1i2.jpg",
-      "src/assets/a1i3.jpg",
-      "src/assets/a1i4.jpg",
-      "src/assets/a1i5.jpg",
-      "src/assets/a1i6.jpg",
-      "src/assets/a1i7.jpg",
-      "src/assets/a1i8.jpg",
-      "src/assets/a1i9.jpg",
-      "src/assets/a1i10.jpg",
-    ],
+    images: [a1i1, a1i2, a1i3, a1i4, a1i5, a1i6, a1i7, a1i8, a1i9, a1i10],
   },
   {
     title: "Appartamento Scirocco",
     description:
       "Appartamento con due camere matrimoniali, con esterno, bagno privato e cucina.",
-    images: [
-      "src/assets/a2i1.jpg",
-      "src/assets/a2i2.jpg",
-      "src/assets/a2i3.jpg",
-      "src/assets/a2i4.jpg",
-      "src/assets/a2i5.jpg",
-      "src/assets/a2i6.jpg",
-      "src/assets/a2i7.jpg",
-      "src/assets/a2i8.jpg",
-      "src/assets/a2i9.jpg",
-      "src/assets/a2i10.jpg",
-    ],
+    images: [a2i1, a2i2, a2i3, a2i4, a2i5, a2i6, a2i7, a2i8, a2i9, a2i10],
   },
   {
     title: "Palazzotto Levante",
     description: "Palazzotto da 5 posti letto.",
     images: [
-      "src/assets/a3i1.jpg",
-      "src/assets/a3i2.jpg",
-      "src/assets/a3i3.jpg",
-      "src/assets/a3i4.jpg",
-      "src/assets/a3i5.jpg",
-      "src/assets/a3i6.jpg",
-      "src/assets/a3i7.jpg",
-      "src/assets/a3i8.jpg",
-      "src/assets/a3i9.jpg",
-      "src/assets/a3i10.jpg",
-      "src/assets/a3i11.jpg",
-      "src/assets/a3i12.jpg",
-      "src/assets/a3i13.jpg",
+      a3i1,
+      a3i2,
+      a3i3,
+      a3i4,
+      a3i5,
+      a3i6,
+      a3i7,
+      a3i8,
+      a3i9,
+      a3i10,
+      a3i11,
+      a3i12,
+      a3i13,
     ],
   },
 ];
@@ -71,15 +88,14 @@ const App: React.FC = () => {
       {/* Apartments Section */}
       <Container sx={{ py: 4 }}>
         <div id="apartments">
-          {apartments.map(({ title, description, images }) => {
-            return (
-              <ApartmentSection
-                title={title}
-                description={description}
-                images={images}
-              />
-            );
-          })}
+          {apartments.map(({ title, description, images }, index) => (
+            <ApartmentSection
+              key={index}
+              title={title}
+              description={description}
+              images={images}
+            />
+          ))}
         </div>
       </Container>
 
