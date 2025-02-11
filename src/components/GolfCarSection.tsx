d5dd1bf175ff340712b7d696b4d18058bf2b7aa9
LEONARDO MICELLI <leonardomicelli@gmail.com>
2025-02-11 14:58:43 +0100

    fix: center slider

--- src/components/GolfCarSection.tsx
+++ src/components/GolfCarSection.tsx
@@ -8,6 +8,7 @@ import {
   Card,
   CardMedia,
   CardContent,
+  Paper,
 } from "@mui/material";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
@@ -42,59 +43,38 @@ const GolfcarsSection: React.FC<GolfcarSectionProps> = ({
         Discover our range of high-quality golfcars, rented by Phidea, designed
         to offer comfort and style.
       </Typography>
-      <Container>
-        <Grid container spacing={4}>
-          <Grid item xs={12} sm={6} md={4} key={startIndex}>
-            <Card sx={{ maxWidth: 345, mx: "auto", boxShadow: 3 }}>
-              {images.length > 1 ? (
-                // Se ci sono più immagini, usa uno Swiper slider
-                <Swiper
-                  spaceBetween={10}
-                  slidesPerView={1}
-                  style={{ width: "100%", height: "200px" }}
-                >
-                  {images.map((img, idx) => (
-                    <SwiperSlide key={idx}>
-                      <CardMedia
-                        component="img"
-                        height="200"
-                        image={img}
-                        alt={`${name} - image ${idx + 1}`}
-                        sx={{
-                          width: "100%",
-                          height: "100%",
-                          objectFit: fitStyle,
-                          objectPosition: "center",
-                        }}
-                      />
-                    </SwiperSlide>
-                  ))}
-                </Swiper>
-              ) : (
-                // Altrimenti, mostra una singola immagine
-                <CardMedia
-                  component="img"
-                  height="200"
-                  image={images[0]}
-                  alt={name}
-                  sx={{
-                    objectFit: "cover",
-                    objectPosition: "center", // Centra l'immagine
-                  }}
-                />
-              )}
-              <CardContent>
-                <Typography variant="h6" gutterBottom>
-                  {name}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {description}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        </Grid>
-      </Container>
+      <Paper
+        sx={{
+          maxWidth: 600,
+          mx: "auto",
+          borderRadius: 2,
+          overflow: "hidden",
+        }}
+        elevation={3}
+      >
+        <Swiper
+          spaceBetween={10}
+          slidesPerView={1}
+          style={{ width: "100%", height: "300px" }}
+        >
+          {images.map((img, idx) => (
+            <SwiperSlide key={idx}>
+              <CardMedia
+                component="img"
+                height="200"
+                image={img}
+                alt={`${name} - image ${idx + 1}`}
+                sx={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: fitStyle,
+                  objectPosition: "center",
+                }}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Paper>
     </Box>
   );
 };
